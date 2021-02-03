815eb0e5b21ef707e496ecece107edd1ce5d4029
Jonas <1390095882@qq.com>
2021-02-03 22:50:38 +0800

    PaintedCat@20210203

--- PythonProgramTest/test.py
+++ PythonProgramTest/test.py
@@ -18,8 +18,17 @@ def compress(raw: str) -> bytes:
             ans = ans + '\\x{:02X}'.format(index) +type
             type = ss
             index = 1
-    ans = ans + '\\x{:02X}'.format(index) + type
-    print(ans.encode('utf-8'))
+    ans = ans + hex(index) + type
+    # ans = ans + '\\x{:02X}'.format(index) + type
+    print(ans)
+    # print(ans.encode('utf-8'))
+    # print(bytes.fromhex(hex(index)))
+    bb = bytes(str(ans), encoding = "utf8")
+    print(bb[1:])
+       
+
+        
+
     return bytes(ans, encoding = "gbk")
     # return bytes(map(ord, ans))
 
@@ -27,6 +36,10 @@ if __name__ == "__main__":
     # execute only if run as a script
     print(compress("abc"))
     print('\\x{:02X}'.format(16))
+    print(isinstance('\\x{:02X}'.format(16),str))
+    bb = '\x01a\x01b\x01c'
+    # print('18'.decode(hex))
+    print(bytes(bb, encoding = "utf8"))
     # print(binascii.unhexlify(compress("abc")))
     # print("zhang")
 
