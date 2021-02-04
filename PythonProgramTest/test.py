805935461135dc4e7700bd7b77285dfd3345a57c
work <1390095882@qq.com>
2021-02-04 17:26:36 +0800

    PaintedCat@20210204

--- PythonProgramTest/test.py
+++ PythonProgramTest/test.py
@@ -9,37 +9,40 @@ def compress(raw: str) -> bytes:
 
     index = 0
     type = raw[0]
-    ans = ''
+    ans = b''
 
     for ss in raw:
         if ss == type:
             index = index + 1
         else:
-            ans = ans + '\\x{:02X}'.format(index) +type
+            ans = ans + bytes.fromhex ('{:02X}'.format(index)) +bytes(str(type), encoding = "utf8")
             type = ss
             index = 1
-    ans = ans + hex(index) + type
-    # ans = ans + '\\x{:02X}'.format(index) + type
-    print(ans)
-    # print(ans.encode('utf-8'))
-    # print(bytes.fromhex(hex(index)))
-    bb = bytes(str(ans), encoding = "utf8")
-    print(bb[1:])
-       
 
-        
+    ans = ans + bytes.fromhex ('{:02X}'.format(index)) +bytes(str(type), encoding = "utf8")
 
-    return bytes(ans, encoding = "gbk")
-    # return bytes(map(ord, ans))
+    return ans
+
 
 if __name__ == "__main__":
     # execute only if run as a script
-    print(compress("abc"))
-    print('\\x{:02X}'.format(16))
-    print(isinstance('\\x{:02X}'.format(16),str))
-    bb = '\x01a\x01b\x01c'
-    # print('18'.decode(hex))
-    print(bytes(bb, encoding = "utf8"))
+    # print("\N{GRINNING FACE} \N{SHAMROCK}")
+    Unicodestring = "\N{GRINNING FACE} \N{SHAMROCK}"
+    # print( str(Unicodestring, "ISO-8859-1") )
+    print( Unicodestring.decode('gbk') )
+    print(compress("\N{GRINNING FACE} \N{SHAMROCK}"))
+    print(b'\x01\xf0\x01\x9f\x01\x98\x01\x80\x01 \x01\xe2\x02\x98')
+    # AssertionError: b'\x01\xf0\x9f\x98\x80\x01 \x01\xe2\x98\x98' != b'\x01\xf0\x01\x9f\x01\x98\x01\x80\x01 \x01\xe2\x02\x98' : compress('😀 ☘') must handle multibyte Unicode chars
+
+    ss = "acbb  bcccc"
+    print(ss)
+    print(compress(ss))
+    # print('\\x{:02X}'.format(16))
+    # print(isinstance('\\x{:02X}'.format(16),str))
+    # bb = '\x01a\x01b\x01c'
+    # # print('18'.decode(hex))
+    # print(bb)
+    # print( )
     # print(binascii.unhexlify(compress("abc")))
     # print("zhang")
 
