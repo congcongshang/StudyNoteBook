600562a98853a33465a95f80c1d34222e839fcdb
work <1390095882@qq.com>
2021-08-25 17:55:27 +0800

    PaintedCat@20210825

--- CProgramTest/Cprimer/base.c
+++ CProgramTest/Cprimer/base.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 // 注释 我是
+// 系统库 <>
+// 用户自定义 ""
+void butler(void);
+
 int main(){
-    printf("hello world");
+    int dogs;
+    printf("How many dogs do you have?\n");
+    dogs = 34;
+    printf("So you have %d dog(s)\n",dogs);
     return 0;
+}
+
+void butler(void){
+    printf("You rang, sir?\n");
 }
