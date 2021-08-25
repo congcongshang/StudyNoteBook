f5cf38f495ec370ad61652bce4739c52f0bb517e
Jonas <1390095882@qq.com>
2021-08-25 22:30:14 +0800

    PaintedCat@20210825

--- CProgramTest/Cprimer/base.c
+++ CProgramTest/Cprimer/base.c
@@ -1,17 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 // 注释 我是
 // 系统库 <>
 // 用户自定义 ""
-void butler(void);
+void p(char *s);
 
-int main(){
+int main()
+{
     int dogs;
     printf("How many dogs do you have?\n");
     dogs = 34;
-    printf("So you have %d dog(s)\n",dogs);
+    printf("So you have %d dog(s)\n", dogs);
+    p("shang");
     return 0;
 }
 
-void butler(void){
-    printf("You rang, sir?\n");
+void p(char *s)
+{
+    printf("%s\n",s);
 }
